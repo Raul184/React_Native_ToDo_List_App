bb86c8922fcba0ba55429633b185303e709ecfa0
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-06-09 23:23:36 +0100

    login form set

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [ name , setName ] = useState('Test')
+  const [ age , setAge ] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Please provide your name:</Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='Your name'
+        onChangeText={val => setName(val)}
+      />
+      <Text>Please provide your age:</Text>
+      <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='Your name'
+        onChangeText={val => setAge(val)}
+      />
     </View>
   );
 }
@@ -16,4 +31,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1 ,
+    borderColor: '#777',
+    padding: 8 ,
+    margin:10,
+    width: 200
+  }
 });
